5236f9e07f1981b18e054468315a9af5e2b0e2d5
ow <bablanov.dauren@yandex.kz>
2017-12-18 16:55:18 +0600

    add singleton

---
+++ singleton/Preferences.php
@@ -0,0 +1,27 @@
+<?php
+
+class Preferences
+{
+    private $props = [];
+    private static $instance;
+
+    private function __construct() { }
+
+    public static function getInstance()
+    {
+        if (empty(self::$instance)) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function setProperty($key, $val)
+    {
+        $this->props[$key] = $val;
+    }
+
+    public function getProperty($key)
+    {
+        return $this->props[$key];
+    }
+}
